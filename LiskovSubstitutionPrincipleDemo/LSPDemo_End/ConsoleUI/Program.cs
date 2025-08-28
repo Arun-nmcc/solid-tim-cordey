5cd7c2aaf9f8059149bbf5fe13c73d839af3ae8a
Arun Rethina <arun.rethina@finos.tech>
2025-08-28 13:11:50 +0530

    d

---
+++ LiskovSubstitutionPrincipleDemo/LSPDemo_End/ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IManager accountingVP = new CEO(); // we can replace with new manager() because both are IManager
+
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
+
+            IManaged emp = new Manager(); // we can replace with new Employee() because both are IManaged
+
+            emp.FirstName = "Tim";
+            emp.LastName = "Corey";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingleResponsibilityPrincipleDemo-tim-yt/ConsoleUI/Program.cs
@@ -0,0 +1,77 @@
+﻿public class PersonModel {
+    private String firstName;
+    private String lastName;
+
+    public PersonModel(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
+java
+Copy
+Edit
+public class EmployeeModel {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    public EmployeeModel(String firstName, String lastName, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+}
+java
+Copy
+Edit
+public class Accounts {
+    public EmployeeModel create(PersonModel person) {
+        String email = (person.getFirstName().substring(0, 1) + person.getLastName() + "@acme.com").toLowerCase();
+        return new EmployeeModel(person.getFirstName(), person.getLastName(), email);
+    }
+}
+java
+Copy
+Edit
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        List<PersonModel> applicants = new ArrayList<>();
+        applicants.add(new PersonModel("Tim", "Cory"));
+        applicants.add(new PersonModel("Sue", "Storm"));
+        applicants.add(new PersonModel("Nancy", "Romo"));
+
+        Accounts accountProcessor = new Accounts();
+        List<EmployeeModel> employees = new ArrayList<>();
+
+        for (PersonModel applicant : applicants) {
+            EmployeeModel employee = accountProcessor.create(applicant);
+            employees.add(employee);
+
+            System.out.println(employee.getFirstName() + " " + employee.getLastName() +
+                    " - " + employee.getEmailAddress());
+        }
+    }
+}
